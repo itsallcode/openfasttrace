3c6f2fccb2fa0d38df6168b61bbd7ef77a2f2b5e
kaklakariada <kaklakariada@users.noreply.github.com>
2017-07-02 15:18:39 +0200

    Upgrade dependencies, fix deprecation warning

--- src/test/java/openfasttrack/importer/markdown/TestImportMarkdown.java
+++ src/test/java/openfasttrack/importer/markdown/TestImportMarkdown.java
@@ -36,7 +36,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import openfasttrack.core.SpecificationItemId;
 import openfasttrack.importer.ImportEventListener;

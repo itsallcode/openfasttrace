1a276a76e5a8c63e995b143f8076289b057861f2
redcatbear <redcatbear@ursus-minor.de>
2017-08-13 10:26:43 +0200

    69: Fixed failing test.
    Task-Url: https://github.com/hamstercommunity/openfasttrack/issues/69

--- src/test/java/openfasttrack/cli/TestCliStarter.java
+++ src/test/java/openfasttrack/cli/TestCliStarter.java
@@ -201,7 +201,7 @@ public class TestCliStarter
     {
         expectCliExitWithAssertions(() -> {
             assertOutputFileExists(true);
-            assertThat(getOutputFileContent().length(), greaterThan(600));
+            assertThat(getOutputFileContent().length(), greaterThan(400));
         });
         runCliStarter(TRACE_COMMAND, OUTPUT_FILE_PARAMETER, this.outputFile.toString());
     }

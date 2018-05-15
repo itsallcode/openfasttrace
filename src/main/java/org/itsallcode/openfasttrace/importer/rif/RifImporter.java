e6fc55caf92255d301a0a8826ec27edc6704c134
Sascha Ziesig <sascha@ziesig.net>
2018-05-15 09:37:12 +0200

    added skeleton for RIF importer as requested in #132

---
+++ src/main/java/org/itsallcode/openfasttrace/importer/rif/RifImporter.java
@@ -0,0 +1,15 @@
+package org.itsallcode.openfasttrace.importer.rif;
+
+import org.itsallcode.openfasttrace.importer.Importer;
+
+public class RifImporter implements Importer
+{
+
+    @Override
+    public void runImport()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/org/itsallcode/openfasttrace/importer/rif/RifImporterFactory.java
@@ -0,0 +1,30 @@
+package org.itsallcode.openfasttrace.importer.rif;
+
+import java.io.Reader;
+
+import javax.xml.parsers.SAXParserFactory;
+
+import org.itsallcode.openfasttrace.core.xml.SaxParserConfigurator;
+import org.itsallcode.openfasttrace.importer.ImportEventListener;
+import org.itsallcode.openfasttrace.importer.Importer;
+import org.itsallcode.openfasttrace.importer.RegexMatchingImporterFactory;
+
+public class RifImporterFactory extends RegexMatchingImporterFactory
+{
+    private final SAXParserFactory saxParserFactory;
+
+    public RifImporterFactory()
+    {
+        super("(?i).*\\.(rif)");
+        this.saxParserFactory = SaxParserConfigurator.createSaxParserFactory();
+    }
+
+    @Override
+    public Importer createImporter(final String fileName, final Reader reader,
+            final ImportEventListener listener)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

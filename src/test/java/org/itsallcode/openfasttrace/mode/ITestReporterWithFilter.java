3988ce7974adfcb46735559b195f73c982a2517b
redcatbear <redcatbear@ursus-minor.de>
2018-07-09 21:11:44 +0200

    #145: Removed superfluous lines in Markdown test sample for tag filtering. Added missing license header.
    Task-Url: https://github.com/itsallcode/openfasttrace/issues/145

--- src/test/java/org/itsallcode/openfasttrace/mode/ITestReporterWithFilter.java
+++ src/test/java/org/itsallcode/openfasttrace/mode/ITestReporterWithFilter.java
@@ -1,5 +1,27 @@
 package org.itsallcode.openfasttrace.mode;
 
+/*-
+ * #%L
+ * OpenFastTrace
+ * %%
+ * Copyright (C) 2016 - 2018 itsallcode.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
@@ -23,31 +45,18 @@ public class ITestReporterWithFilter
 {
     public static final String SPECIFICATION = String.join(System.lineSeparator(), //
             "`feat~a~1`", //
-            "", //
             "I am a feature", //
-            "", //
             "`req~b~2`", //
-            "", //
             "A user requirement", //
-            "", //
             "Tags: tag1, tag2", //
-            "", //
             "`dsn~c~3`", //
-            "", //
             "Design", //
-            "", //
             "Tags: tag1, tag3", //
-            "", //
             "`impl~d~4`", //
-            "", //
             "An implementation", //
-            "", //
             "Tags: tag2, tag3", //
-            "", //
             "`utest~e~5`", //
-            "", //
             "A unit test", //
-            "", //
             "Tags: tag3");
 
     @Rule

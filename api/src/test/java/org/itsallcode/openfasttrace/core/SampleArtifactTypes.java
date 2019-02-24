dfeef990b298c8cc9a17924e5f3065f554148013
kaklakariada <kaklakariada@users.noreply.github.com>
2019-02-24 14:06:00 +0100

    #221: Fix sonar warning

--- api/src/test/java/org/itsallcode/openfasttrace/core/SampleArtifactTypes.java
+++ api/src/test/java/org/itsallcode/openfasttrace/core/SampleArtifactTypes.java
@@ -24,6 +24,11 @@ package org.itsallcode.openfasttrace.core;
 
 public class SampleArtifactTypes
 {
+    private SampleArtifactTypes()
+    {
+        // Not instantiable
+    }
+
     public static final String ARCH = "arch";
     public static final String DSN = "dsn";
     public static final String IMPL = "impl";

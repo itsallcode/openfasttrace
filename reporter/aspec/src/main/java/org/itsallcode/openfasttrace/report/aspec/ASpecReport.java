6521d6ddf73bcbe91facd1224a0c9b59c8eec4b6
Bernd Haberstumpf <bernd.haberstumpf@audi.de>
2021-02-06 17:36:25 +0100

    Added initial version of aspeoc reporter. Initial version is mainly a copy of the Specobject exporter. No tests yet.

---
+++ reporter/aspec/src/main/java/org/itsallcode/openfasttrace/report/aspec/ASpecReport.java
@@ -0,0 +1,247 @@
+package org.itsallcode.openfasttrace.report.aspec;
+
+/*-
+ * #%L
+ * OpenFastTrace
+ * %%
+ * Copyright (C) 2016 - 2018 itsallcode.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import org.itsallcode.openfasttrace.api.core.*;
+import org.itsallcode.openfasttrace.api.exporter.ExporterException;
+import org.itsallcode.openfasttrace.api.report.Reportable;
+import org.itsallcode.openfasttrace.api.report.ReporterContext;
+
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+
+public class ASpecReport implements Reportable {
+    private static final Logger LOG = Logger.getLogger(ASpecReport.class.getName());
+
+    private final Trace trace;
+    private final XMLOutputFactory xmlOutputFactory;
+    private final Newline newline;
+
+    /**
+     * Create a new instance of an {@link ASpecReport}
+     *
+     * @param trace trace to be reported on
+     */
+    public ASpecReport(final Trace trace, final ReporterContext context) {
+        this.trace = trace;
+        this.newline = context.getSettings().getNewline();
+        this.xmlOutputFactory = XMLOutputFactory.newFactory();
+    }
+
+
+    @Override
+    public void renderToStream(final OutputStream outputStream) {
+        final XMLStreamWriter xmlWriter = createXmlWriter(new OutputStreamWriter(outputStream));
+        final IndentingXMLStreamWriter indentingXmlWriter = new IndentingXMLStreamWriter(xmlWriter);
+        final Map<String, List<LinkedSpecificationItem>> items = groupByDoctype(this.trace.getItems().stream());
+
+        LOG.info( "aspec starting");
+        try (indentingXmlWriter) {
+            writeOutput(indentingXmlWriter, items);
+        } catch (final XMLStreamException e) {
+            throw new ExporterException("Generating document", e);
+        }
+    }
+
+    private XMLStreamWriter createXmlWriter(final Writer writer) {
+        try {
+            return this.xmlOutputFactory.createXMLStreamWriter(writer);
+        } catch (final XMLStreamException e) {
+            throw new ExporterException("Error creating xml stream writer for writer " + writer, e);
+        }
+    }
+
+    private Map<String, List<LinkedSpecificationItem>> groupByDoctype(
+            final Stream<LinkedSpecificationItem> itemStream) {
+        return itemStream.collect(
+                groupingBy(LinkedSpecificationItem::getArtifactType, LinkedHashMap::new, toList()));
+    }
+
+    private void writeOutput(final XMLStreamWriter writer, Map<String, List<LinkedSpecificationItem>> items) throws XMLStreamException {
+        writer.writeStartDocument("UTF-8", "1.0");
+        writer.writeStartElement("specdocument");
+
+        for (final Map.Entry<String, List<LinkedSpecificationItem>> entry : items.entrySet()) {
+            final String doctype = entry.getKey();
+            final List<LinkedSpecificationItem> specItems = entry.getValue();
+            writeItems(writer, doctype, specItems);
+        }
+
+        writeStatistics(writer);
+        writer.writeEndElement();
+        writer.writeEndDocument();
+    }
+
+    private static final Comparator<LinkedSpecificationItem> LINKED_ITEM_BY_ID = Comparator
+            .comparing(LinkedSpecificationItem::getId);
+
+    private void writeStatistics(final XMLStreamWriter writer) throws XMLStreamException {
+        writer.writeStartElement("summary");
+        writeElement(writer, "count", trace.count());
+        writeElement(writer, "countDefects", trace.countDefects());
+        writer.writeStartElement("defects");
+        for ( final LinkedSpecificationItem item : trace.getDefectItems().stream()
+                .sorted(LINKED_ITEM_BY_ID).collect(Collectors.toCollection(LinkedList::new)) ) {
+            printDefectItem(writer,item);
+        }
+        writer.writeEndElement();
+        writer.writeEndElement();
+    }
+
+    private void printDefectItem(final XMLStreamWriter writer, final LinkedSpecificationItem item) throws XMLStreamException {
+        writer.writeStartElement("defectobject");
+        writeElement(writer, "id", item.getId().toString());
+        writer.writeEndElement();
+    }
+
+    private void writeItems(final XMLStreamWriter writer, final String doctype, final List<LinkedSpecificationItem> specItems)
+            throws XMLStreamException {
+        LOG.finest(() -> "Writing " + specItems.size() + " items with doctype " + doctype);
+        writer.writeStartElement("specobjects");
+        writer.writeAttribute("doctype", doctype);
+        for (final LinkedSpecificationItem item : specItems) {
+            writeItem(writer, item);
+        }
+        writer.writeEndElement();
+    }
+
+    private void writeItem(final XMLStreamWriter writer, final LinkedSpecificationItem item) throws XMLStreamException {
+        final String description = processMultilineText(item.getDescription());
+        final String rationale = processMultilineText(item.getItem().getRationale());
+        final String comment = processMultilineText(item.getItem().getComment());
+        writer.writeStartElement("specobject");
+        writeElement(writer, "id", item.getName());
+        writeElementIfPresent(writer, "shortdesc", item.getTitle());
+        writeElement(writer, "status", item.getStatus().toString());
+        writeElement(writer, "version", item.getRevision());
+        writeLocation(writer, item.getLocation());
+        writeElementIfPresent(writer, "description", description);
+        writeElementIfPresent(writer, "rationale", rationale);
+        writeElementIfPresent(writer, "comment", comment);
+
+        writeTags(writer, item.getTags());
+        writeNeedsArtifactTypes(writer, item.getNeedsArtifactTypes());
+        writeCoveredIds(writer, item.getCoveredIds());
+        writeDependsOnIds(writer, item.getItem().getDependOnIds());
+
+        writer.writeEndElement();
+    }
+
+    private String processMultilineText(final String text) {
+        return unifyNewlines(text);
+    }
+
+    private String unifyNewlines(final String text) {
+        final Matcher matcher = Newline.anyNewlinePattern().matcher(text);
+        return matcher.replaceAll(this.newline.toString());
+    }
+
+    private void writeTags(final XMLStreamWriter writer, final List<String> tags) throws XMLStreamException {
+        if (tags.isEmpty()) {
+            return;
+        }
+        writer.writeStartElement("tags");
+        for (final String tag : tags) {
+            writeElement(writer, "tag", tag);
+        }
+        writer.writeEndElement();
+    }
+
+    private void writeDependsOnIds(final XMLStreamWriter writer, final List<SpecificationItemId> dependOnIds)
+            throws XMLStreamException {
+        if (dependOnIds.isEmpty()) {
+            return;
+        }
+        writer.writeStartElement("dependencies");
+        for (final SpecificationItemId dependsOnId : dependOnIds) {
+            writeElement(writer, "dependson", dependsOnId.toString());
+        }
+        writer.writeEndElement();
+    }
+
+    private void writeCoveredIds(final XMLStreamWriter writer, final List<SpecificationItemId> coveredIds)
+            throws XMLStreamException {
+        if (coveredIds.isEmpty()) {
+            return;
+        }
+        writer.writeStartElement("providescoverage");
+        for (final SpecificationItemId coveredId : coveredIds) {
+            writer.writeStartElement("provcov");
+            writeElement(writer, "linksto", coveredId.getArtifactType() + ":" + coveredId.getName());
+            writeElement(writer, "dstversion", coveredId.getRevision());
+            writer.writeEndElement();
+        }
+        writer.writeEndElement();
+    }
+
+    private void writeNeedsArtifactTypes(final XMLStreamWriter writer, final List<String> needsArtifactTypes)
+            throws XMLStreamException {
+        if (needsArtifactTypes.isEmpty()) {
+            return;
+        }
+        writer.writeStartElement("needscoverage");
+        for (final String neededArtifactType : needsArtifactTypes) {
+            writeElement(writer, "needsobj", neededArtifactType);
+        }
+        writer.writeEndElement();
+    }
+
+    private void writeElement(final XMLStreamWriter writer, final String elementName, final int content) throws XMLStreamException {
+        writeElement(writer, elementName, String.valueOf(content));
+    }
+
+    private void writeElementIfPresent(final XMLStreamWriter writer, final String elementName, final String content)
+            throws XMLStreamException {
+        if (content != null && !content.isEmpty()) {
+            writeElement(writer, elementName, content);
+        }
+    }
+
+    private void writeElement(final XMLStreamWriter writer, final String elementName, final String content)
+            throws XMLStreamException {
+        writer.writeStartElement(elementName);
+        writer.writeCharacters(content);
+        writer.writeEndElement();
+    }
+
+    private void writeLocation(final XMLStreamWriter writer, final Location location) throws XMLStreamException {
+        if (location != null && location.getPath() != null && !location.getPath().isEmpty()) {
+            writeElement(writer, "sourcefile", location.getPath());
+            writeElement(writer, "sourceline", location.getLine());
+        }
+    }
+
+
+}
---
+++ reporter/aspec/src/main/java/org/itsallcode/openfasttrace/report/aspec/DelegatingXMLStreamWriter.java
@@ -0,0 +1,233 @@
+package org.itsallcode.openfasttrace.report.aspec;
+
+/*-
+ * #%L
+ * OpenFastTrace Specobject Exporter
+ * %%
+ * Copyright (C) 2016 - 2019 itsallcode.org
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
+class DelegatingXMLStreamWriter implements XMLStreamWriter
+{
+    private final XMLStreamWriter writer;
+
+    public DelegatingXMLStreamWriter(XMLStreamWriter writer)
+    {
+        this.writer = writer;
+    }
+
+    @Override
+    public void writeStartElement(String localName) throws XMLStreamException
+    {
+        writer.writeStartElement(localName);
+    }
+
+    @Override
+    public void writeStartElement(String namespaceURI, String localName) throws XMLStreamException
+    {
+        writer.writeStartElement(namespaceURI, localName);
+    }
+
+    @Override
+    public void writeStartElement(String prefix, String localName, String namespaceURI)
+            throws XMLStreamException
+    {
+        writer.writeStartElement(prefix, localName, namespaceURI);
+    }
+
+    @Override
+    public void writeEmptyElement(String namespaceURI, String localName) throws XMLStreamException
+    {
+        writer.writeEmptyElement(namespaceURI, localName);
+    }
+
+    @Override
+    public void writeEmptyElement(String prefix, String localName, String namespaceURI)
+            throws XMLStreamException
+    {
+        writer.writeEmptyElement(prefix, localName, namespaceURI);
+    }
+
+    @Override
+    public void writeEmptyElement(String localName) throws XMLStreamException
+    {
+        writer.writeEmptyElement(localName);
+    }
+
+    @Override
+    public void writeEndElement() throws XMLStreamException
+    {
+        writer.writeEndElement();
+    }
+
+    @Override
+    public void writeEndDocument() throws XMLStreamException
+    {
+        writer.writeEndDocument();
+    }
+
+    @Override
+    public void close() throws XMLStreamException
+    {
+        writer.close();
+    }
+
+    @Override
+    public void flush() throws XMLStreamException
+    {
+        writer.flush();
+    }
+
+    @Override
+    public void writeAttribute(String localName, String value) throws XMLStreamException
+    {
+        writer.writeAttribute(localName, value);
+    }
+
+    @Override
+    public void writeAttribute(String prefix, String namespaceURI, String localName, String value)
+            throws XMLStreamException
+    {
+        writer.writeAttribute(prefix, namespaceURI, localName, value);
+    }
+
+    @Override
+    public void writeAttribute(String namespaceURI, String localName, String value)
+            throws XMLStreamException
+    {
+        writer.writeAttribute(namespaceURI, localName, value);
+    }
+
+    @Override
+    public void writeNamespace(String prefix, String namespaceURI) throws XMLStreamException
+    {
+        writer.writeNamespace(prefix, namespaceURI);
+    }
+
+    @Override
+    public void writeDefaultNamespace(String namespaceURI) throws XMLStreamException
+    {
+        writer.writeDefaultNamespace(namespaceURI);
+    }
+
+    @Override
+    public void writeComment(String data) throws XMLStreamException
+    {
+        writer.writeComment(data);
+    }
+
+    @Override
+    public void writeProcessingInstruction(String target) throws XMLStreamException
+    {
+        writer.writeProcessingInstruction(target);
+    }
+
+    @Override
+    public void writeProcessingInstruction(String target, String data) throws XMLStreamException
+    {
+        writer.writeProcessingInstruction(target, data);
+    }
+
+    @Override
+    public void writeCData(String data) throws XMLStreamException
+    {
+        writer.writeCData(data);
+    }
+
+    @Override
+    public void writeDTD(String dtd) throws XMLStreamException
+    {
+        writer.writeDTD(dtd);
+    }
+
+    @Override
+    public void writeEntityRef(String name) throws XMLStreamException
+    {
+        writer.writeEntityRef(name);
+    }
+
+    @Override
+    public void writeStartDocument() throws XMLStreamException
+    {
+        writer.writeStartDocument();
+    }
+
+    @Override
+    public void writeStartDocument(String version) throws XMLStreamException
+    {
+        writer.writeStartDocument(version);
+    }
+
+    @Override
+    public void writeStartDocument(String encoding, String version) throws XMLStreamException
+    {
+        writer.writeStartDocument(encoding, version);
+    }
+
+    @Override
+    public void writeCharacters(String text) throws XMLStreamException
+    {
+        writer.writeCharacters(text);
+    }
+
+    @Override
+    public void writeCharacters(char[] text, int start, int len) throws XMLStreamException
+    {
+        writer.writeCharacters(text, start, len);
+    }
+
+    @Override
+    public String getPrefix(String uri) throws XMLStreamException
+    {
+        return writer.getPrefix(uri);
+    }
+
+    @Override
+    public void setPrefix(String prefix, String uri) throws XMLStreamException
+    {
+        writer.setPrefix(prefix, uri);
+    }
+
+    @Override
+    public void setDefaultNamespace(String uri) throws XMLStreamException
+    {
+        writer.setDefaultNamespace(uri);
+    }
+
+    @Override
+    public void setNamespaceContext(NamespaceContext context) throws XMLStreamException
+    {
+        writer.setNamespaceContext(context);
+    }
+
+    @Override
+    public NamespaceContext getNamespaceContext()
+    {
+        return writer.getNamespaceContext();
+    }
+
+    @Override
+    public Object getProperty(String name)
+    {
+        return writer.getProperty(name);
+    }
+}

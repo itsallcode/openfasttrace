1b7ccab4ce59718a4bd5a04505424a625d832d57
kaklakariada <kaklakariada@users.noreply.github.com>
2022-08-14 16:07:19 +0200

    Add missing module-info for aspec reporter

---
+++ reporter/aspec/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * This provides an report generator for the Aspec format.
+ */
+module org.itsallcode.openfasttrace.report.aspec
+{
+    exports org.itsallcode.openfasttrace.report.aspec;
+
+    requires java.xml;
+    requires java.logging;
+    requires org.itsallcode.openfasttrace.api;
+    requires org.itsallcode.openfasttrace.exporter.common;
+}

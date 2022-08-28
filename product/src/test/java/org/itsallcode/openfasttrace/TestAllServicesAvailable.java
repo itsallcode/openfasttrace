172d41e270ff45ae0fe71ddf9586b6bd0cd85c29
kaklakariada <kaklakariada@users.noreply.github.com>
2022-08-28 11:29:15 +0200

    #336: Use service loader

--- product/src/test/java/org/itsallcode/openfasttrace/TestAllServicesAvailable.java
+++ product/src/test/java/org/itsallcode/openfasttrace/TestAllServicesAvailable.java
@@ -1,6 +1,5 @@
 package org.itsallcode.openfasttrace;
 
-import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
@@ -9,16 +8,12 @@ import static org.mockito.Mockito.withSettings;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.Optional;
-import java.util.ServiceLoader;
 import java.util.stream.Stream;
 
 import org.itsallcode.openfasttrace.api.core.Newline;
 import org.itsallcode.openfasttrace.api.core.Trace;
 import org.itsallcode.openfasttrace.api.exporter.Exporter;
 import org.itsallcode.openfasttrace.api.exporter.ExporterContext;
-import org.itsallcode.openfasttrace.api.exporter.ExporterFactory;
 import org.itsallcode.openfasttrace.api.importer.ImportEventListener;
 import org.itsallcode.openfasttrace.api.importer.Importer;
 import org.itsallcode.openfasttrace.api.importer.ImporterContext;
@@ -27,7 +22,10 @@ import org.itsallcode.openfasttrace.api.importer.input.InputFile;
 import org.itsallcode.openfasttrace.api.importer.input.RealFileInput;
 import org.itsallcode.openfasttrace.api.report.Reportable;
 import org.itsallcode.openfasttrace.api.report.ReporterContext;
-import org.itsallcode.openfasttrace.api.report.ReporterFactory;
+import org.itsallcode.openfasttrace.core.exporter.ExporterFactoryLoader;
+import org.itsallcode.openfasttrace.core.importer.ImporterFactoryLoader;
+import org.itsallcode.openfasttrace.core.report.ReporterFactoryLoader;
+import org.itsallcode.openfasttrace.core.serviceloader.InitializingServiceLoader;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.io.TempDir;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -36,52 +34,41 @@ import org.mockito.Answers;
 
 class TestAllServicesAvailable
 {
-    private static List<ImporterFactory> importerFactories;
-    private static List<ExporterFactory> exporterFactories;
-    private static List<ReporterFactory> reporterFactories;
+    private static ImporterFactoryLoader importerLoader;
+    private static ExporterFactoryLoader exporterLoader;
+    private static ReporterFactoryLoader reporterLoader;
 
     @TempDir
     static Path tempDir;
 
     @BeforeAll
-    static void loadFactories()
+    static void createFactoryLoaders()
     {
-        importerFactories = loadImporterFactories();
-        exporterFactories = loadExporterFactories();
-        reporterFactories = loadReporterFactories();
+        importerLoader = createImporterLoader();
+        exporterLoader = createExporterLoader();
+        reporterLoader = createReporterLoader();
     }
 
-    private static List<ImporterFactory> loadImporterFactories()
+    private static ImporterFactoryLoader createImporterLoader()
     {
-        final ServiceLoader<ImporterFactory> loader = ServiceLoader.load(ImporterFactory.class);
         final ImporterContext contextMock = mock(ImporterContext.class,
                 withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS));
-        final List<ImporterFactory> importerFactories = loader.stream() //
-                .map(ServiceLoader.Provider::get).collect(toList());
-        importerFactories.forEach(factory -> factory.init(contextMock));
-        return importerFactories;
+
+        return new ImporterFactoryLoader(InitializingServiceLoader.load(ImporterFactory.class, contextMock));
     }
 
-    private static List<ExporterFactory> loadExporterFactories()
+    private static ExporterFactoryLoader createExporterLoader()
     {
-        final ServiceLoader<ExporterFactory> loader = ServiceLoader.load(ExporterFactory.class);
         final ExporterContext contextMock = mock(ExporterContext.class,
                 withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS));
-        final List<ExporterFactory> exporterFactories = loader.stream() //
-                .map(ServiceLoader.Provider::get).collect(toList());
-        exporterFactories.forEach(factory -> factory.init(contextMock));
-        return exporterFactories;
+        return new ExporterFactoryLoader(contextMock);
     }
 
-    private static List<ReporterFactory> loadReporterFactories()
+    private static ReporterFactoryLoader createReporterLoader()
     {
-        final ServiceLoader<ReporterFactory> loader = ServiceLoader.load(ReporterFactory.class);
         final ReporterContext contextMock = mock(ReporterContext.class,
                 withSettings().defaultAnswer(Answers.RETURNS_DEEP_STUBS));
-        final List<ReporterFactory> reporterFactories = loader.stream() //
-                .map(ServiceLoader.Provider::get).collect(toList());
-        reporterFactories.forEach(factory -> factory.init(contextMock));
-        return reporterFactories;
+        return new ReporterFactoryLoader(contextMock);
     }
 
     @ParameterizedTest
@@ -90,14 +77,12 @@ class TestAllServicesAvailable
     void importerAvailable(final String suffix)
     {
         final InputFile file = RealFileInput.forPath(Paths.get("file." + suffix));
-        final Optional<ImporterFactory> factory = importerFactories.stream() //
-                .filter(f -> f.supportsFile(file)) //
-                .findAny();
-        if (factory.isEmpty())
+        if (!importerLoader.supportsFile(file))
         {
             fail("No importer found for file name suffix '" + suffix + "'");
         }
-        final Importer importer = factory.get().createImporter(file, mock(ImportEventListener.class));
+        final Importer importer = importerLoader.getImporterFactory(file).get().createImporter(file,
+                mock(ImportEventListener.class));
         assertNotNull(importer);
     }
 
@@ -106,14 +91,12 @@ class TestAllServicesAvailable
     { "specobject" })
     void exporterAvailable(final String format)
     {
-        final Optional<ExporterFactory> factory = exporterFactories.stream() //
-                .filter(f -> f.supportsFormat(format)) //
-                .findAny();
-        if (factory.isEmpty())
+        if (!exporterLoader.isFormatSupported(format))
         {
             fail("No exporter found for format '" + format + "'");
         }
-        final Exporter exporter = factory.get().createExporter(tempDir.resolve("file." + format), format,
+        final Exporter exporter = exporterLoader.getExporterFactory(format).createExporter(
+                tempDir.resolve("file." + format), format,
                 StandardCharsets.UTF_8,
                 Newline.UNIX, Stream.empty());
         assertNotNull(exporter);
@@ -124,14 +107,11 @@ class TestAllServicesAvailable
     { "aspec", "html", "plain" })
     void reporterAvailable(final String format)
     {
-        final Optional<ReporterFactory> factory = reporterFactories.stream() //
-                .filter(f -> f.supportsFormat(format)) //
-                .findAny();
-        if (factory.isEmpty())
+        if (!reporterLoader.isFormatSupported(format))
         {
             fail("No reporter found for format '" + format + "'");
         }
-        final Reportable reportable = factory.get().createImporter(Trace.builder().build());
+        final Reportable reportable = reporterLoader.getReporterFactory(format).createImporter(Trace.builder().build());
         assertNotNull(reportable);
     }
 }

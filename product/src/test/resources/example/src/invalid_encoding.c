020a559776079b2036dec343e9912b26a063aa0a
kaklakariada <christoph@users.sourceforge.net>
2024-02-23 17:47:43 +0100

    #391: Fix reading files with invalid encoding

---
+++ product/src/test/resources/example/src/invalid_encoding.c
@@ -0,0 +1,6 @@
+Test case for https://github.com/itsallcode/openfasttrace/issues/391
+This is based on https://xenbits.xen.org/gitweb/?p=xen.git;a=blob;f=xen/drivers/video/font_8x14.c;h=e80d178c3869a4b506313cdea19c76b18fe2b4c2;hb=HEAD
+
+Ensure to not modify the rest of this file when editing it!
+
+/* 128 0x80 'Ÿˆ */

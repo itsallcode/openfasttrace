c17cfcc19c817c76548acc94bca070d6ebef542f
kaklakariada <kaklakariada@users.noreply.github.com>
2024-06-02 12:24:46 +0200

    Add shellcheck

--- .github/workflows/github_release.sh
+++ .github/workflows/github_release.sh
@@ -8,7 +8,7 @@ base_dir="$( cd "$(dirname "$0")/../.." >/dev/null 2>&1 ; pwd -P )"
 pom_file="$base_dir/parent/pom.xml"
 
 # Read project version from pom file
-project_version=$(grep "<revision>" $pom_file | sed --regexp-extended 's/<.{0,1}revision>//g' | xargs)
+project_version=$(grep "<revision>" "$pom_file" | sed --regexp-extended 's/<.{0,1}revision>//g' | xargs)
 echo "Read project version '$project_version' from $pom_file"
 
 # Calculate checksum
@@ -24,9 +24,9 @@ popd
 
 # Create GitHub release
 changes_file="$base_dir/doc/changes/changes_${project_version}.md"
-echo Changes file: $changes_file
+echo "Changes file: $changes_file"
 title="Release $project_version"
-notes=$(cat $changes_file)
+notes=$(cat "$changes_file")
 tag="$project_version"
-release_url=$(gh release create --latest --draft --title "$title" --notes "$notes" --target main $tag $artifact $checksum_file_path)
+release_url=$(gh release create --latest --draft --title "$title" --notes "$notes" --target main "$tag" "$artifact" "$checksum_file_path")
 echo "Release URL: $release_url"

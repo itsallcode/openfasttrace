ff68cde363c0146b69bced4e3015a6e5dc6fc226
kaklakariada <christoph@users.sourceforge.net>
2024-06-16 18:14:09 +0200

    Fix unit test

--- core/src/test/java/org/itsallcode/openfasttrace/core/serviceloader/ClassPathServiceLoaderTest.java
+++ core/src/test/java/org/itsallcode/openfasttrace/core/serviceloader/ClassPathServiceLoaderTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.*;
@@ -45,13 +46,26 @@ class ClassPathServiceLoaderTest
         final DummyServiceImpl service = new DummyServiceImpl();
         when(serviceLoaderMock.stream()).thenReturn(Stream.of(providerMock));
         when(providerMock.get()).thenReturn(service);
+        when(originMock.getClassLoader()).thenReturn(DummyServiceImpl.class.getClassLoader());
         final List<DummyService> services = new ClassPathServiceLoader<DummyService>(originMock, serviceLoaderMock)
-                .load()
-                .toList();
+                .load().toList();
         assertAll(() -> assertThat(services, hasSize(1)),
                 () -> assertThat(services.get(0), not(nullValue())),
                 () -> assertThat(services.get(0), instanceOf(DummyServiceImpl.class)),
                 () -> assertThat(services.get(0), sameInstance(service)));
+    }
+
+    @Test
+    void loadingIgnoresServicesFromOtherClassLoaders(@Mock final ServiceLoader<DummyService> serviceLoaderMock,
+            @Mock final Provider<DummyService> providerMock, @Mock final ServiceOrigin originMock)
+    {
+        final DummyServiceImpl service = new DummyServiceImpl();
+        when(serviceLoaderMock.stream()).thenReturn(Stream.of(providerMock));
+        when(providerMock.get()).thenReturn(service);
+        when(originMock.getClassLoader()).thenReturn(mock(ClassLoader.class));
+        final List<DummyService> services = new ClassPathServiceLoader<DummyService>(originMock, serviceLoaderMock)
+                .load().toList();
+        assertThat(services, empty());
     }
 
     static interface DummyService

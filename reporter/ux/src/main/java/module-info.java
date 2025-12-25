85be76a8b1f0884af311d4f1cf31e12644ba90dc
Bernd Haberstumpf <poldi@thatswing.de>
2025-12-25 22:38:34 +0100

    Fixed imports in module-info.java of ux reporter.

--- reporter/ux/src/main/java/module-info.java
+++ reporter/ux/src/main/java/module-info.java
@@ -7,6 +7,10 @@ module org.itsallcode.openfasttrace.report.ux
 {
     requires transitive org.itsallcode.openfasttrace.api;
     requires java.logging;
+    requires java.desktop;
+    requires jdk.jfr;
+    requires java.xml.crypto;
+    requires java.xml;
 
     provides org.itsallcode.openfasttrace.api.report.ReporterFactory
             with org.itsallcode.openfasttrace.report.ux.UxReporterFactory;

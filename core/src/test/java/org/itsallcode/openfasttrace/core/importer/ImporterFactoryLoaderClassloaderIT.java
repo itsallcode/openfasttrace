309697218bcd6ef0ecdcbab48add0e758606c52f
redcatbaer <rcb4gh@proton.me>
2026-07-11 10:05:13 +0200

    536: Improved mocking and readability of ImporterFactoryLoaderClassloaderIT.

--- core/src/test/java/org/itsallcode/openfasttrace/core/importer/ImporterFactoryLoaderClassloaderIT.java
+++ core/src/test/java/org/itsallcode/openfasttrace/core/importer/ImporterFactoryLoaderClassloaderIT.java
@@ -11,39 +11,40 @@ import org.itsallcode.openfasttrace.api.importer.ImporterException;
 import org.itsallcode.openfasttrace.api.importer.input.InputFile;
 import org.itsallcode.openfasttrace.api.importer.input.RealFileInput;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 /**
  * Test to reproduce the classloader mismatch problem when discovering
  * importers.
  * <p>
- * This scenario will happen if OFT is used as a library with a custom classloader.
- * What we want in that case is a very clear exception message because without
- * an importer, neither tracing nor converting have any chance to work.
+ * This scenario will happen if OFT is used as a library with a custom
+ * classloader. What we want in that case is a very clear exception message
+ * because without an importer, neither tracing nor converting does have any
+ * chance to work.
  * </p>
  */
+@ExtendWith(MockitoExtension.class)
 class ImporterFactoryLoaderClassloaderIT
 {
     /**
-     * This test demonstrates that if the Thread Context ClassLoader (TCCL) 
-     * does not have access to the importer factories, {@link ImporterFactoryLoader} 
+     * This test demonstrates that if the Thread Context ClassLoader (TCCL) does
+     * not have access to the importer factories, {@link ImporterFactoryLoader}
      * will fail to find any importers and should throw an exception.
      */
     @Test
-    final void testGetImporterFactoryThrowsExceptionWhenTcclIsLimited()
+    final void testGetImporterFactoryThrowsExceptionWhenTcclIsLimited(@Mock final ImporterContext context)
     {
         final ClassLoader originalTccl = Thread.currentThread().getContextClassLoader();
         final ClassLoader limitedClassLoader = new NoOpClassLoader();
-
         try
         {
             Thread.currentThread().setContextClassLoader(limitedClassLoader);
-            final ImporterContext context = Mockito.mock(ImporterContext.class);
             final ImporterFactoryLoader loaderWithLimitedTccl = new ImporterFactoryLoader(context);
             final InputFile file = RealFileInput.forPath(Paths.get("test.md"));
-            final ImporterException exception = assertThrows(ImporterException.class, () ->
-                loaderWithLimitedTccl.getImporterFactory(file)
-            );
+            final ImporterException exception = assertThrows(ImporterException.class,
+                    () -> loaderWithLimitedTccl.getImporterFactory(file));
             assertThat(exception.getMessage().contains("No importers discovered"), is(true));
         }
         finally

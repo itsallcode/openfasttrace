edde843600242cf84d83e21c63b8facc32cfc166
kaklakariada <christoph@users.sourceforge.net>
2026-07-23 09:26:51 +0200

    Improve test coverage

--- importer/tag-importer-common/src/test/java/org/itsallcode/openfasttrace/importer/tag/common/TestLineReader.java
+++ importer/tag-importer-common/src/test/java/org/itsallcode/openfasttrace/importer/tag/common/TestLineReader.java
@@ -1,11 +1,20 @@
 package org.itsallcode.openfasttrace.importer.tag.common;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
+import org.itsallcode.openfasttrace.api.importer.ImporterException;
 import org.itsallcode.openfasttrace.api.importer.input.InputFile;
 import org.itsallcode.openfasttrace.api.importer.input.RealFileInput;
 import org.itsallcode.openfasttrace.importer.tag.common.LineReader.LineConsumer;
@@ -84,6 +93,34 @@ class TestLineReader {
     void testReadLinesTwoLinesWithLFCR() {
         readContent("line1\r\nline2");
         assertLinesRead("line1", "line2");
+    }
+
+    @Test
+    void wrapsConsumerFailureWithLineInformation() {
+        final RuntimeException cause = new IllegalArgumentException("invalid line");
+        doThrow(cause).when(this.consumerMock).readLine(1, "line1");
+
+        final ImporterException exception = assertThrows(ImporterException.class, () -> readContent("line1"));
+
+        assertAll(
+                () -> assertThat(exception.getMessage(),
+                        equalTo("Error processing line dummy:1 'line1': " + cause)),
+                () -> assertThat(exception.getCause(), sameInstance(cause)));
+    }
+
+    @Test
+    void wrapsReaderCreationFailure() throws IOException {
+        final InputFile file = mock(InputFile.class);
+        final IOException cause = new IOException("cannot read");
+        when(file.createReader()).thenThrow(cause);
+        when(file.toString()).thenReturn("unreadable.file");
+
+        final ImporterException exception = assertThrows(ImporterException.class,
+                () -> LineReader.create(file).readLines(this.consumerMock));
+
+        assertAll(
+                () -> assertThat(exception.getMessage(), equalTo("Error reading \"unreadable.file\" at line 0")),
+                () -> assertThat(exception.getCause(), sameInstance(cause)));
     }
 
     private void readContent(final String content) {

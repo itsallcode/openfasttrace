80989f49ae37d88b90b312b26226fdee391a2f60
kaklakariada <christoph@users.sourceforge.net>
2026-07-23 09:46:59 +0200

    Simplify unit test

--- importer/tag-importer-common/src/test/java/org/itsallcode/openfasttrace/importer/tag/common/TestCoverageTagParser.java
+++ importer/tag-importer-common/src/test/java/org/itsallcode/openfasttrace/importer/tag/common/TestCoverageTagParser.java
@@ -1,68 +1,70 @@
 package org.itsallcode.openfasttrace.importer.tag.common;
 
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.nio.file.Paths;
+import java.util.List;
 
+import org.itsallcode.openfasttrace.api.core.SpecificationItem;
 import org.itsallcode.openfasttrace.api.core.SpecificationItemId;
-import org.itsallcode.openfasttrace.api.importer.ImportEventListener;
+import org.itsallcode.openfasttrace.api.importer.SpecificationListBuilder;
 import org.itsallcode.openfasttrace.api.importer.input.InputFile;
 import org.itsallcode.openfasttrace.api.importer.tag.config.PathConfig;
 import org.itsallcode.openfasttrace.importer.tag.common.LineReader.LineConsumer;
 import org.itsallcode.openfasttrace.testutil.importer.input.StreamInput;
 import org.junit.jupiter.api.Test;
-import org.mockito.InOrder;
 
-class TestCoverageTagParser
-{
+class TestCoverageTagParser {
     private static final String FILE = "source.file";
 
     @Test
-    void testImportsFullTag()
-    {
-        final ImportEventListener listener = mock(ImportEventListener.class);
+    void importsFullTag() {
+        final SpecificationListBuilder listener = SpecificationListBuilder.create();
         final LineConsumer parser = CoverageTagParser.create(null, inputFile(), listener);
 
         final String coverageTag = "[" + "impl~name~1->dsn~covered~2>>utest]";
         parser.readLine(3, coverageTag);
 
-        final InOrder inOrder = inOrder(listener);
-        inOrder.verify(listener).beginSpecificationItem();
-        inOrder.verify(listener).setLocation(FILE, 3);
-        inOrder.verify(listener).setId(SpecificationItemId.parseId("impl~name~1"));
-        inOrder.verify(listener).addCoveredId(SpecificationItemId.parseId("dsn~covered~2"));
-        inOrder.verify(listener).addNeededArtifactType("utest");
-        inOrder.verify(listener).endSpecificationItem();
-        inOrder.verifyNoMoreInteractions();
+        assertThat(listener.build(), equalTo(List.of(item(SpecificationItemId.parseId("impl~name~1"), 3,
+                List.of("dsn~covered~2"), List.of("utest")))));
     }
 
     @Test
-    void testImportsConfiguredShortTag()
-    {
-        final PathConfig config = mock(PathConfig.class);
-        when(config.getCoveredItemArtifactType()).thenReturn("req");
-        when(config.getCoveredItemNamePrefix()).thenReturn("prefix.");
-        when(config.getTagArtifactType()).thenReturn("utest");
-        final ImportEventListener listener = mock(ImportEventListener.class);
+    void importsConfiguredShortTag() {
+        final PathConfig config = pathConfig();
+        final SpecificationListBuilder listener = SpecificationListBuilder.create();
         final LineConsumer parser = CoverageTagParser.create(config, inputFile(), listener);
 
         parser.readLine(2, "[[covered:3]]");
 
-        final InOrder inOrder = inOrder(listener);
-        inOrder.verify(listener).beginSpecificationItem();
-        inOrder.verify(listener).setLocation(FILE, 2);
-        inOrder.verify(listener).setId(SpecificationItemId.createId("utest", "prefix.covered-1743877134"));
-        inOrder.verify(listener).addCoveredId(SpecificationItemId.createId("req", "prefix.covered", 3));
-        inOrder.verify(listener).endSpecificationItem();
-        inOrder.verifyNoMoreInteractions();
+        assertThat(listener.build(),
+                equalTo(List.of(item(SpecificationItemId.createId("utest", "prefix.covered-1743877134"),
+                        2, List.of("req~prefix.covered~3"), List.of()))));
     }
 
-    private static InputFile inputFile()
-    {
+    private static PathConfig pathConfig() {
+        return PathConfig.builder()
+                .patternPathMatcher("glob:**")
+                .coveredItemArtifactType("req")
+                .coveredItemNamePrefix("prefix.")
+                .tagArtifactType("utest")
+                .build();
+    }
+
+    private static SpecificationItem item(final SpecificationItemId id, final int lineNumber,
+            final List<String> coveredIds, final List<String> neededArtifactTypes) {
+        final SpecificationItem.Builder builder = SpecificationItem.builder()
+                .id(id)
+                .location(FILE, lineNumber);
+        coveredIds.stream().map(SpecificationItemId::parseId).forEach(builder::addCoveredId);
+        neededArtifactTypes.forEach(builder::addNeedsArtifactType);
+        return builder.build();
+    }
+
+    private static InputFile inputFile() {
         return StreamInput.forReader(Paths.get(FILE), new BufferedReader(new StringReader("")));
     }
 }
